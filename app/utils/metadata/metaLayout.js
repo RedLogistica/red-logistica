cb335ce74e4eed8ccaf81404357465e138ab3af4
Red Logística <164713552+RedLogistica@users.noreply.github.com>
2024-09-18 16:02:50 +0000

    update OG

--- app/utils/metadata/metaLayout.js
+++ app/utils/metadata/metaLayout.js
@@ -36,19 +36,26 @@ export const layoutMetadata = {
       "logística e-commerce",
     ],
     creator: "Red Logística",
-    metadataBase: new URL("https://redlogistica.github.io/red-logistica/"),
+    metadataBase: new URL("https://red-logistica.org/"),
     title: { template: "%s | Red Logística", default: "Red Logística" },
     description:
       "Red Logística: Radio, entrevistas, noticias y más sobre logística. Tu fuente de información y entretenimiento sobre el mundo logístico",
-    openGraph: {
-      title: "Red Logística: Radio, entrevistas, noticias y más sobre logística",
-      description:
-        "Tu fuente de información y entretenimiento sobre el mundo logístico.",
-      url: "https://redlogistica.github.io/red-logistica/",
-      siteName: "Red Logística",
-      locale: "es-es",
-      type: "website",
-    },
+      openGraph: {
+        title: "Red Logística: Radio, entrevistas, noticias y más sobre logística",
+        description: "Tu fuente de información y entretenimiento sobre el mundo logístico.",
+        url: "https://red-logistica.org/",
+        siteName: "Red Logística",
+        locale: "es-es",
+        type: "website",
+        images: [
+          {
+            url: "https://red-logistica.org/opengraph-image.png", // URL de tu imagen
+            width: 500, // Ancho de la imagen
+            height: 500,  // Alto de la imagen
+            alt: "Logo de Red Logística"  // Texto alternativo de la imagen
+          }
+        ]
+      },
     robots: {
       index: false,
       follow: true,
@@ -57,5 +64,5 @@ export const layoutMetadata = {
     alternates: {
       canonical: "/",
     },
-    manifest: "https://redlogistica.github.io/red-logistica/manifest.json",
+    manifest: "https://red-logistica.org/manifest.json",
   };
